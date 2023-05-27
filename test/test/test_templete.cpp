c4eabb700df919915fa4b21fb9b735666f99927a
Rahul Singh Bhauryal <rahulbhauryal3@gmail.com>
2023-05-27 10:41:45 +0200

    Add gtest

---
+++ test/test/test_templete.cpp
@@ -0,0 +1,24 @@
+#include <gtest/gtest.h>
+#include "../../src/app/play01Templates.h"
+
+// Test class templete
+TEST(TempleteTest, ClassTemplateTest) {
+    ClassTemplate<int> playClassTemplate(100);
+    ASSERT_EQ(playClassTemplate.read(), 100);
+
+    playClassTemplate.write(50);
+    ASSERT_EQ(playClassTemplate.read(), 50);
+}
+
+
+// Test class templete using unique pointer
+TEST(TempleteTest, ClassTemplateTestUniquePointer) {
+    std::string testString = "Hi Test";
+    std::unique_ptr<ClassTemplate<std::string>> playClassTemplate;
+    playClassTemplate = std::make_unique<ClassTemplate<std::string>>(testString);
+    ASSERT_EQ(playClassTemplate->read(), testString);
+
+    testString = "Hi Test again";
+    playClassTemplate->write(testString);
+    ASSERT_EQ(playClassTemplate->read(), testString);
+}
